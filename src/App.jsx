1bb1718dc19367726e7d6e6ddd727144812e2eb0
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-03-18 22:20:21 +0600

    updated the app.jsx and added env file and added it to git ignore, added database and post data from it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './app.css';
+import { getDatabase, set, ref } from 'firebase/database';
+import { app } from './firebase';
+
+const db = getDatabase(app);
+
 function App() {
+  const putData = () => {
+    set(ref(db, 'users/rakib'), { id: 1, name: 'rakib hasan', age: 18 });
+    alert('putted data successfully');
+  };
+
   return (
     <div className="App">
       <h1>Firebase React App</h1>
-      <button>Put Data</button>
+      <button onClick={putData}>Put Data</button>
     </div>
   );
 }
